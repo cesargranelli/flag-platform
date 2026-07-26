ab1b19bf4180c6180e5302704d6e8cfc2a28b2a5
Cesar Granelli <cesargranelli@gmail.com>
2026-07-26 11:32:31 -0300

    feat: add Spring Boot main application class

---
+++ backend/src/main/java/br/com/flagplatform/FlagPlatformApplication.java
@@ -0,0 +1,12 @@
+package br.com.flagplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlagPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlagPlatformApplication.class, args);
+    }
+}
